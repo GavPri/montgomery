4e4dcfd22f78da442bec0ee4dbd8caef53f17bbb
Gavin Prior <120742516+GavPri@users.noreply.github.com>
2026-04-08 14:26:48 +0000

    create home portfolio page

---
+++ app/portfolio/page.tsx
@@ -0,0 +1,13 @@
+import { Feature284 } from "@/components/feature284";
+
+;
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-screen flex-col items-center justify-center py-16 px-4 lg:px-16">
+        <Feature284/>
+      </main>
+    </div>
+  );
+}
