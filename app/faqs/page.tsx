0c7cd4035d2c7157ce80ed469f74fc6b095961c1
Gavin Prior <120742516+GavPri@users.noreply.github.com>
2026-04-08 14:26:37 +0000

    create faq page

---
+++ app/faqs/page.tsx
@@ -0,0 +1,12 @@
+import { Faq20 } from "@/components/faq20";
+
+
+export default function Home() {
+  return (
+    <div className="flex flex-col flex-1 items-center justify-center font-sans">
+      <main className="flex flex-1 w-screen flex-col items-center justify-center py-16 px-4 lg:px-16">
+        <Faq20/>
+      </main>
+    </div>
+  );
+}
